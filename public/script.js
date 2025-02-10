f345b680b42b163f5b64d1e593be05eefe5a28a5
Sayedbuhari10 <150268106+Sayedbuhari10@users.noreply.github.com>
2025-02-10 15:11:29 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -15,7 +15,7 @@ function checkForPalindrome (input ) {
   resultDiv.replaceChildren();
 
   const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
-  let resultMsg ="kishor is a gay";
+  let resultMsg ="shreeharan is a gay";
 
   const pTag = document.createElement('p');
   pTag.className = 'user-input';
